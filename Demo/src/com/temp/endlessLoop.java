9bfd8c144fd449484c395d5272fbcd3abeb8bed8
THEJOkerheart <1354170042@qq.com>
2023-01-06 19:28:57 +0800

    1

---
+++ Demo/src/com/temp/endlessLoop.java
@@ -0,0 +1,11 @@
+package com.temp;
+
+public class endlessLoop {
+    //git
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i>0;i++){
+            System.out.println("循环中。。。");
+        }
+    }
+}
